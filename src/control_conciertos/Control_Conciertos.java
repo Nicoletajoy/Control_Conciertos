912fe1fec1634f05ca25d220380482800cda90d4
dam2 <dam2@a223e06.dc.domenicoscarlatti.es>
2024-10-15 16:39:06 +0200

    un cambio

---
+++ src/control_conciertos/Control_Conciertos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package control_conciertos;
+
+/**
+ *
+ * @author dam2
+ */
+public class Control_Conciertos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola aqui vamos");
+    }
+    
+}
